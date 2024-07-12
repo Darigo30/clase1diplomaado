103ad91c36349b993b8d4026cc9f739120acad08
Dariana <dariana.gomez@uc.cl>
2024-07-11 21:27:44 -0400

    esta es la primera clase del diplomado

---
+++ index.js
@@ -0,0 +1,74 @@
+//variables
+// console.log("Hola Diplomado FullStack")
+//Variables
+//LET
+// let nombre = "Juan"
+// nombre = "Pedro"
+
+// console.log(nombre)
+
+// if(true) {
+//     let nombre = "Carlos"
+//     console.log(nombre)
+// }
+
+// console.log(nombre)
+
+//Const
+
+// const apellido = "Gomez"
+// apellido = "Perez"
+// console.log(apellido)
+
+//Operadores 
+
+// let a = 5
+// let b = 3
+
+// let suma = a + b 
+
+// console.log(suma)
+
+//IF ELSE
+
+// let hora = 12
+
+// if (hora < 12) {
+//     console.log("Buenos días")
+// } else {
+//     console.log("Buenas tardes")
+// }
+
+
+//FOR
+// let numero = 10;
+
+// for (let i = 0;  i < numero; i++) {
+//     console.log(i)
+// }
+
+// let nombres = ["Juan", "Pedro", "Carlos"]
+
+// for(let i = 0; i < nombres.length; i++) {
+//     console.log(nombres[i])
+// }
+
+//Funciones declarativa
+
+// const saludo = (nombre) => {
+//     let mensaje = "Hola"
+//     console.log(`${mensaje} ${nombre}`)
+// }
+// saludo("Dariana")
+
+// function miFuncion() {
+//     let mensaje = "Hola Clase"
+//     console.log(mensaje)
+// }
+// miFuncion()
+
+//Funciones autoejecutables 
+
+(function(){
+    console.log("Hola Diplomado FullStack")
+})();
